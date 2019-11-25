54d9749ce11c20d613e254123e5ab22df9d9617b
Jeffrey Ilar <jilar@petzent.com>
2019-11-25 15:27:00 -0800

    Box Object Model, FlexBox, and Position Layout Handling

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,14 +3,40 @@ import {View,Text,StyleSheet} from 'react-native';
 
 const BoxScreen=()=>{
   return(
-    <View>
-      <Text></Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textOneStyle}>Child 1</Text>
+      <Text style={styles.textTwoStyle}>Child 2</Text>
+      <Text style={styles.textThreeStyle}>Child 3</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  viewStyle:{
+    flexDirection:'row',
+    height:200,
+    flex:1
+  },
+  textOneStyle:{
+    borderWidth:3,
+    borderColor: 'red',
+    height:100,
+    flex:1
+  },
+  textTwoStyle:{
+    borderWidth:3,
+    borderColor: 'green',
+    height:100,
+    top: 100,
+    flex:1
 
+  },
+  textThreeStyle:{
+    borderWidth:3,
+    borderColor: 'purple',
+    height:100,
+    flex:1
+  }
 });
 
 export default BoxScreen;
